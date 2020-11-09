cbb7578333df44827b5298bedf45b83231e442d0
Kyle <gouldk4@gmail.com>
2020-11-09 12:21:29 -0500

    Added Navbar and refactored Homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import { Login, Register, Profile } from './components/index';
-import { SearchContainer, DetailsContainer } from './containers/index';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
+import React from "react";
+import "./App.css";
+import { Login, Register, Profile } from "./components/index";
+import { SearchContainer, DetailsContainer } from "./containers/index";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 // for small Playlist widget || used on User Profile page
-import playListReducer from './reducers/playListReducer';
+import playListReducer from "./reducers/playListReducer";
 // for large Playlist display || used on PlaylistDetails page || includes inner track details
-import DetailsReducer from './reducers/DetailsReducer';
+import DetailsReducer from "./reducers/DetailsReducer";
 
 const allReducer = combineReducers({
   playListReducer,
@@ -21,19 +21,23 @@ function App() {
   return (
     <Provider store={store}>
       <Router>
-        <Link to="/Details">Details</Link>
+        {/* <Link to="/Details">Details</Link>
         <Link to="/Home">Home</Link>
         <Link to="/Login">Login</Link>
         <Link to="/Register">Register</Link>
         <Link to="/Profile">Profile</Link>
-        <Link to="/Search">Search</Link>
+        <Link to="/Search">Search</Link> */}
         <Route
           path="/Details/:playlistId"
           render={(props) => (
-            <DetailsContainer {...props} playlistId={props.match.params.playlistId} />
+            <DetailsContainer
+              {...props}
+              playlistId={props.match.params.playlistId}
+            />
           )}
         />
-        <Route path="/Home" component={SearchContainer.Home} />
+        <Route path="/" exact component={SearchContainer.Home} />
+        <Route path="/Home" exact component={SearchContainer.Home} />
         <Route path="/Login" component={Login} />
         <Route path="/Register" component={Register} />
         <Route path="/Profile" component={Profile} />
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,88 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faUserCircle,
+  faSearch,
+  faSignInAlt,
+  faUserPlus,
+  faListOl,
+} from "@fortawesome/free-solid-svg-icons";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// Import in this order for responsive nav toggle to work!
+import "../../../node_modules/jquery/dist/jquery.min.js";
+import "../../../node_modules/bootstrap/dist/js/bootstrap.min.js";
+
+import exampleImg from "../../assets/damon.jpg";
 
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <nav class="navbar sticky-top navbar-dark bg-dark navbar-expand-md shadow-lg ">
+      <span class="navbar-brand font-weight-bold">(logo)</span>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse " id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto ">
+          <li class="nav-item">
+            <Link class="nav-link" to="/Home">
+              <FontAwesomeIcon icon={faHome} />
+              &nbsp; Home
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/Profile">
+              <FontAwesomeIcon icon={faUserCircle} />
+              &nbsp; Profile
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/Search">
+              <FontAwesomeIcon icon={faSearch} />
+              &nbsp; Search
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/Login">
+              <FontAwesomeIcon icon={faSignInAlt} />
+              &nbsp; Login
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/Register">
+              <FontAwesomeIcon icon={faUserPlus} />
+              &nbsp; Register
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/Details">
+              <FontAwesomeIcon icon={faListOl} />
+              &nbsp; Details
+            </Link>
+          </li>
+        </ul>
+
+        <span className="float-right text-white">
+          <img
+            style={{ height: "35px" }}
+            className="rounded-circle mr-2"
+            src={exampleImg}
+          />
+          (username)
+        </span>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
