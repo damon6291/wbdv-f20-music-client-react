e34ab81e58fb5a8f9dccce74a057a581f11af72a
Hussein Mohamed <mohamed.h@husky.neu.edu>
2020-12-09 12:34:01 -0500

    small fix for the footer

--- src/components/Privacy/Privacy.js
+++ src/components/Privacy/Privacy.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const privacyFooter = () => {
     return (
-        <footer style={{position:"absolute", bottom:"0px"}}>
+        <footer style={{position:"relative", bottom:"0px"}}>
             Our Privacy Protocol aligns with MIT's privacy protocol. If at any time one does not want their information stored, you are free to delete your account and any information given to us will be deleted. 
             <a href="/Privacy"> Our Privacy Protocol</a>
         </footer>
