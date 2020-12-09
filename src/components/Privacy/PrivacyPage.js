38249881bae5541c9a817003d9582e278e91f5b9
damon6291 <damon@Sangmins-MacBook-Pro.local>
2020-12-09 14:16:38 -0500

    heroku

--- src/components/Privacy/PrivacyPage.js
+++ src/components/Privacy/PrivacyPage.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 const PrivacyPage = () => {
-    return (
-        <div>
-            <h1>Privacy Protocol</h1>
-            <p>Any information provided to us through the Spotify API is what we use in our application. If at any time you wish to stop sharing your information with us you may delete your account and we will delete any information on our end regarding your profile.
-                We align our practices heavily with <a href="https://web.mit.edu/privacy/"> MIT's Privacy Protocol</a>. We thank you for your support!
-            </p>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <h1>Privacy Protocol</h1>
+      <p>
+        Any information provided to us through the Spotify API is what we use in our application. If
+        at any time you wish to stop sharing your information with us you may delete your account
+        and we will delete any information on our end regarding your profile. We align our practices
+        heavily with <a href="https://web.mit.edu/privacy/"> MIT's Privacy Protocol</a>. We thank
+        you for your support!
+      </p>
+    </div>
+  );
+};
 
 export default PrivacyPage;
