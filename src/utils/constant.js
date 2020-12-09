8ce71ea6d60f787e7b4dbb41b69889ad07fc1044
damon6291 <damon@Sangmins-MacBook-Pro.local>
2020-12-09 15:12:43 -0500

    urls

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,7 @@
 //For Deployment
-// export const url = 'https://wbdv-f20-music-server.herokuapp.com/api/';
-// export const clienturl = 'https://wbdv-f20-music.herokuapp.com/';
+export const url = 'https://wbdv-f20-music-server.herokuapp.com/api/';
+export const clienturl = 'https://wbdv-f20-music.herokuapp.com/';
 
 //For test server
-export const url = 'http://localhost:8080/api/';
-export const clienturl = 'http://localhost:3000/';
+// export const url = 'http://localhost:8080/api/';
+// export const clienturl = 'http://localhost:3000/';
