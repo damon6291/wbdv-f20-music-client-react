3591b205a728e86f20f52a5eba049c2f86641d8b
Kyle <gouldk4@gmail.com>
2020-12-13 13:12:29 -0500

    More profile picture fixes

--- src/components/Home/Post.js
+++ src/components/Home/Post.js
@@ -1,29 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import { Playlist } from '../index';
-import Service from '../../services/Services';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from "react";
+import { Playlist } from "../index";
+import Service from "../../services/Services";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
-const Post = ({ post, userId = '', refreshPost, from = '' }) => {
+const Post = ({ post, userId = "", refreshPost, from = "" }) => {
   const [playlist, setPlaylist] = useState([]);
   const [profile, setProfile] = useState([]);
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState("");
 
   useEffect(() => {
     const getData = async () => {
       await Service.getPlaylistInformation(post.playlistId).then((response) =>
         setPlaylist(response)
       );
-      await Service.findProfile(post.userId).then((response) => setProfile(response));
-      await Service.findImage(post.userId).then((response) => setImage(response.images[0].url));
+      await Service.findProfile(post.userId).then((response) =>
+        setProfile(response)
+      );
+      await Service.findImage(post.userId).then((response) => {
+        try {
+          setImage(response.images[0].url);
+        } catch (error) {
+          setImage(
+            "https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg"
+          );
+        }
+      });
     };
     getData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const onRemoveHandler = async () => {
-    await Service.removePost(post._id).then((response) => console.log(response));
+    await Service.removePost(post._id).then((response) =>
+      console.log(response)
+    );
     refreshPost();
   };
 
@@ -32,27 +44,34 @@ const Post = ({ post, userId = '', refreshPost, from = '' }) => {
       <div className="row">
         <div className="col d-flex flex-row align-items-center">
           <img
-            style={{ height: '30px' }}
+            style={{ height: "30px" }}
             alt="user"
             className="rounded-circle mt-1 mr-2"
             src={image}
           />
-          <Link to={`/Profile/${profile._id}`} className="mt-2 font-weight-bold text-dark">
+          <Link
+            to={`/Profile/${profile._id}`}
+            className="mt-2 font-weight-bold text-dark"
+          >
             {profile.displayName}
           </Link>
           {userId === post.userId ? (
-            <FontAwesomeIcon icon={faTimes} className="ml-auto" onClick={() => onRemoveHandler()} />
+            <FontAwesomeIcon
+              icon={faTimes}
+              className="ml-auto"
+              onClick={() => onRemoveHandler()}
+            />
           ) : (
-            ''
+            ""
           )}
         </div>
       </div>
       <div className="row">
         <div className="col">
-          <hr className="m-2" style={{ color: 'black', size: '1px' }}></hr>
+          <hr className="m-2" style={{ color: "black", size: "1px" }}></hr>
           <p>{post.text}</p>
           {/* {console.log(playlist)} */}
-          {from === 'details' ? null : <Playlist playList={playlist} />}
+          {from === "details" ? null : <Playlist playList={playlist} />}
         </div>
       </div>
     </div>
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../../App.css';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import Service from '../../services/Services';
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../../App.css";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import Service from "../../services/Services";
 
 const User = ({
   ownerId: { _id },
@@ -13,9 +13,9 @@ const User = ({
   removeUser,
   refreshProfile,
 }) => {
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const [followers, setFollowers] = useState([]);
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState("");
 
   useEffect(() => {
     console.log(_id);
@@ -24,7 +24,13 @@ const User = ({
       setFollowers(result.followers);
     });
     Service.findImage(_id).then((result) => {
-      setImage(result.images[0].url);
+      try {
+        setImage(result.images[0].url);
+      } catch (error) {
+        setImage(
+          "https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg"
+        );
+      }
     });
   }, [_id]);
 
@@ -43,24 +49,26 @@ const User = ({
         <img src={image} alt="user" className="user-circle-image mr-2" />
         <div className="d-flex flex-column">
           <Link to={`/Profile/${_id}`} className="text-dark">
-            {name ? name : 'Null Name'}
+            {name ? name : "Null Name"}
           </Link>
           <span className="text-secondary text-right">
             <small>{followers.length} followers</small>
           </span>
         </div>
-        {userId === '' || userId === _id ? ( //user is not logged in or user is myself
-          ''
+        {userId === "" || userId === _id ? ( //user is not logged in or user is myself
+          ""
         ) : showFollower ? (
           <button
             className="btn btn-danger h-25 ml-auto px-2 py-1"
-            onClick={() => onFollowHandler()}>
+            onClick={() => onFollowHandler()}
+          >
             Follow
           </button>
         ) : (
           <button
             className="btn btn-danger h-25 ml-auto px-2 py-1"
-            onClick={() => onRemoveHandler()}>
+            onClick={() => onRemoveHandler()}
+          >
             Remove
           </button>
         )}
@@ -73,8 +81,10 @@ const stateToPropertyMapper = (state) => ({
   userId: state.LoginReducer.userId,
 });
 const propertyToDispatchMapper = (dispatch) => ({
-  followUser: (from, to) => Service.followUser(from, to).then((result) => console.log(result)),
-  removeUser: (from, to) => Service.removeFollower(from, to).then((result) => console.log(result)),
+  followUser: (from, to) =>
+    Service.followUser(from, to).then((result) => console.log(result)),
+  removeUser: (from, to) =>
+    Service.removeFollower(from, to).then((result) => console.log(result)),
 });
 
 export default connect(stateToPropertyMapper, propertyToDispatchMapper)(User);
